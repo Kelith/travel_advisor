0f6f47debde17da9ef62ba538d2ea788a827f59a
Kelith <garofalodavide7@gmail.com>
2022-07-16 14:22:38 +0200

    Layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,14 +18,14 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
+          {/* <Autocomplete> */}
             <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
               <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          </Autocomplete>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./MapStyles";
 
 function Map() {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+
   return (
-    <div>Map</div>
-  )
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCr-eq9BpMKlBc5FoPgAaVPfmUqUitDRJ8" }}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultZoom={8}
+        center={{ lat: -34.397, lng: 150.644 }}
+        zoom={isMobile ? 8 : 10}
+        margin={[50, 50, 50, 50]}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          zoomControlOptions: {
+            position: "right",
+          },
+        }}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
